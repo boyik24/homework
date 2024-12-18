92c0fc1f3e78a3cf0aff8ea850d1128661ac7a75
admin <admin@gmail.com>
2024-12-18 20:09:45 +0500

    A

--- file_1.py
+++
@@ -1,7 +0,0 @@
-class Person:
-    def __init__(self,name,phone,email):
-        self.name=name
-        self.phone=phone
-        self.email=email
-    def info(self):
-        print(self.name,self.phone,self.email)
---
+++ file_2.py
@@ -0,0 +1,4 @@
+class Person:
+    def __init__(self,name,phone):
+        self.name=name
+        self.phone=phone
